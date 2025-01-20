7e1663b68e22c7cc11b940665c24a90be302ca55
timkim <timk@adobe.com>
2025-01-19 18:15:42 -0800

    Add in submission block

---
+++ blocks/submission/submission.js
@@ -0,0 +1,20 @@
+import { createTag } from '../../scripts/aem.js';
+
+
+export default function decorate(block) {
+  //const redBlockDiv = createTag('div', { class: 'red-block' });
+  //block.prepend(redBlockDiv);
+  const links = [...block.querySelectorAll('a')].map((a) => a.href);
+  const submitLink = links.find((link) => link.startsWith('https://script.google.com'));
+
+  const submissionFormHTML = `
+    <form 
+      method="POST" 
+      action="${submitLink}}"
+    >
+      <input name="Email" type="email" placeholder="Email" required>
+      <button type="submit">Send</button>
+    </form>`;
+
+    block.innerHTML = submissionFormHTML;
+}
